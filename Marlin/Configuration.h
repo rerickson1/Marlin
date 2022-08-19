13930eb8d98e5307f4b4d1f1280f0c3f0ff4363d
Ryan Erickson <ryan.erickson@lairdconnect.com>
2022-08-18 22:42:53 -0500

    Set baud rate to 1Mbps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -120,7 +120,7 @@
  *
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
-#define BAUDRATE 250000
+#define BAUDRATE 1000000
 
 //#define BAUD_RATE_GCODE     // Enable G-code M575 to set the baud rate
 
@@ -130,7 +130,7 @@
  * :[-2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
  */
 #define SERIAL_PORT_2 0
-#define BAUDRATE_2 250000   // Enable to override BAUDRATE
+#define BAUDRATE_2 1000000   // Enable to override BAUDRATE
 
 /**
  * Select a third serial port on the board to use for communication with the host.
